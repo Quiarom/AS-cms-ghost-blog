7bce8b4b09d26d2a6d6ddb9bf19f0d2c5586b048
Quiarom <userquiarom@gmail.com>
2023-03-24 19:10:06 -0400

    Fix title and description of share buttons

--- components/ShareAsideBar.tsx
+++ components/ShareAsideBar.tsx
@@ -4,13 +4,13 @@ function ShareAsideBar (props: any) {
     return (
         <div className="mx-auto flex w-full max-w-7xl items-start lg:justify-center lg:items-center sm:justify-center gap-3 px-4 pl-0 pt-8">
 
-            <ShareButton url={props.url} description={props.description} shareIn="Clipboard"/>
+            <ShareButton url={props.url} description={`${props.title} \n ${props.description}`} shareIn="Clipboard"/>
 
-            <ShareButton url={props.url} description={props.description} shareIn="Twitter"/>
+            <ShareButton url={props.url} description={`${props.title} \r ${props.description}`} shareIn="Twitter"/>
 
-            <ShareButton url={props.url} description={props.description} shareIn="Linkedin"/>
+            <ShareButton url={props.url} description={`${props.title} \n ${props.description}`} shareIn="Linkedin"/>
 
-            <ShareButton url={props.url} description={props.description} shareIn="Telegram"/>
+            <ShareButton description={`${props.title} \n ${props.description}`} url={props.url} shareIn="Telegram"/>
         </div>
     )
 }
